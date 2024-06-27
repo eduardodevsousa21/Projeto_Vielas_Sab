2bda65648bdd9c2a30ea2bdad6db7e656287675e
unknown <everaldovcjr@gmail.com>
2024-06-27 20:04:25 -0300

    ajustos

---
+++ Projeto_Vielas_Sabado/src/components/Product/index.jsx
@@ -0,0 +1,45 @@
+import style from "./style.module.css"
+import { FormSubmiter } from "../FormSubmiter"
+
+export const Product = (props) => {
+
+    return(
+    <>
+        <section className={style.product} style={props.last == true ? {gridTemplateRows:"repeat(7,auto)"} : {gridTemplateRows:"repeat(6,auto)"}}>
+        
+            <img src={props.img}></img>
+
+            <label>Tipo de mídia:</label>
+            <p>{props.media}</p>
+            <input readOnly={true} type="hidden" value={props.media}/>
+
+            <label>Resolução:</label>
+            <p>{props.resolution}</p>
+            <input readOnly={true} type="hidden" value={props.resolution}/>
+
+            <label className={style.type}>Tipo de licença:</label>
+            <p className={style.type}>{props.type} | Ver resumo das licenças</p>
+            <input readOnly={true} type="hidden" value={props.type}/>
+
+            <label>Como posso utilizar:</label>
+            <p>Disponível para todas as utilizações permitidas ao abrigo dos nossos <a>Termos de Licença</a></p>
+
+            <div className={style.select}> 
+                <input type="radio"/>
+                <label style={{marginLeft:5}}>Selecionar para compra</label>
+            </div>
+            
+            {props.last == true ? 
+            <div className={style.other}>
+                <p style={{marginBlockEnd:20}}>Subtotal: R$ {props.sub}</p>
+                <FormSubmiter text={"prosseguir com a compra"} fontsize={"clamp(1rem,1.5vw,1.25rem)"}/>
+            </div>
+            : 
+            null
+            }
+                    
+        </section>
+    </>
+
+    )
+}
--- Projeto_Vielas_Sabado/src/components/Routes/Purchase/index.jsx
+++ Projeto_Vielas_Sabado/src/components/Routes/Purchase/index.jsx
@@ -1,6 +1,6 @@
 import style from "./style.module.css"
 import Bicicletas from "../../../assets/PurchasePageFotos/bicicletas.jpg"
-import { FormSubmiter } from "../../FormSubmiter"
+import { Product } from "../../Product"
 
 export const Purchase = (props) => {
 
@@ -9,34 +9,16 @@ export const Purchase = (props) => {
             <h2>Carrinho de compras</h2>
             <p>Itens selecionados para compra: 1</p>
 
-            <section className={style.product}>
+            <section className={style.products}>
                 <form>
-                    <img src={Bicicletas}></img>
-
-                    <label>Tipo de mídia:</label>
-                    <p>{props.media}</p>
-                    <input readOnly={true} type="hidden" value={props.media}/>
-
-                    <label>Resolução:</label>
-                    <p>{props.resolution}</p>
-                    <input readOnly={true} type="hidden" value={props.resolution}/>
-
-                    <label className={style.type}>Tipo de licença:</label>
-                    <p className={style.type}>{props.type} | Ver resumo das licenças</p>
-                    <input readOnly={true} type="hidden" value={props.type}/>
-
-                    <label>Como posso utilizar:</label>
-                    <p>Disponível para todas as utilizações permitidas ao abrigo dos nossos <a>Termos de Licença</a></p>
-
-                    <div className={style.select}> 
-                        <input type="radio"/>
-                        <label style={{marginLeft:5}}>Selecionar para compra</label>
-                    </div>
-
-                    <div className={style.other}>
-                        <p style={{marginBlockEnd:20}}>Subtotal: R$ {props.sub}</p>
-                        <FormSubmiter text={"prosseguir com a compra"} fontsize={"clamp(1rem,1.5vw,1.25rem)"}/>
-                    </div>
+                    <Product
+                        type = "Royalty-free"
+                        resolution = "Resolução 4000 x 6000 - 300 dpi - RGB"
+                        media = "Imagem do Creative"
+                        sub = "10,00"
+                        img = {Bicicletas}
+                        last = {true}
+                    />
                 </form>
                 
             </section>
